87a81e667f54d5b5d177f7dc36099e0587ba3abf
Nicolas Goutay <nicolas.goutay@gmail.com>
2020-09-22 14:28:57 +0200

    Refactor: simplify Sanity serializers

--- sanity/serializers.js
+++ sanity/serializers.js
@@ -4,18 +4,8 @@ const h = blocksToHtml.h
 const Sanity = require('./sanityClient')
 
 module.exports = {
-  list: ({ type, children }) => {
-    switch (type) {
-      case 'bullet':
-        return h('ul', {}, children)
-    }
-  },
-  listItem: ({ children }) => {
-    return h('li', {}, children)
-  },
+
   marks: {
-    link: ({ mark, children }) =>
-      h('a', { href: mark.href,}, children),
     inlineimage: ({ mark, children }) => {
       switch (mark._type) {
         case 'inlineimage':
@@ -28,67 +18,12 @@ module.exports = {
     },
   },
   types: {
-    block: ({ node, children }) => {
-      switch (node.style) {
-        case 'h1':
-          return h(
-            'h1',
-            {},
-            children
-          )
-        case 'h2':
-          return h(
-            'h2',
-            {},
-            children
-          )
-        case 'h3':
-          return h(
-            'h3',
-            {},
-            children
-          )
-        case 'normal':
-          return h('p', {}, children)
-        case 'blockquote':
-          return h(
-            'blockquote',
-            {},
-            h(
-              'p',
-              {},
-              children
-            )
-          )
-      }
-    },
     code: ({ node }) => {
       return h(
         'pre',
         {},
         h('code', { className: `language-${node.language}` }, node.code)
       )
-    },
-    image: ({ node }) => {
-      const image = h('img', {
-        src: Sanity.urlFor(Sanity.client, node)
-          .width(node.width)
-          .height(node.height)
-          .url(),
-        alt: node.alt,
-        title: node.title,
-      })
-      const link =
-        node.href && h('a', { href: node.href}, image)
-      const content = h(
-        'div',
-        {
-          style: `width: ${node.width ? node.width + 'px' : '100%'}`,
-        },
-        link || image
-      )
-
-      return content
     },
     instagram: ({ node }) =>
       h('div', { 'data-url': node.url, className: 'instagram my-6' }),
